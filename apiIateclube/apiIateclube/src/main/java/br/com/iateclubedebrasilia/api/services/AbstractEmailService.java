e1e38cc992bdebe55c858392b4fa9def5c32591b
Alex-Buttielie <buttielle@gmail.com>
2021-11-22 22:19:24 -0300

    [012 - refactoring e config ovjeto e-mail]

--- apiIateclube/apiIateclube/src/main/java/br/com/iateclubedebrasilia/api/services/AbstractEmailService.java
+++ apiIateclube/apiIateclube/src/main/java/br/com/iateclubedebrasilia/api/services/AbstractEmailService.java
@@ -1,17 +1,15 @@
 package br.com.iateclubedebrasilia.api.services;
 
-import br.com.iateclubedebrasilia.api.entitys.Usuario;
+import br.com.iateclubedebrasilia.api.domain.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 
 import java.util.Date;
 
-public class AbstractEmailService implements EmailService{
-
+public class AbstractEmailService{
     @Value("${default.sender}")
     private String sender;
 
-    @Override
     public String sendEmail(SimpleMailMessage msg) {
         return null;
     }
@@ -23,7 +21,7 @@ public class AbstractEmailService implements EmailService{
 
     protected SimpleMailMessage prepareNewPasswordEmail(Usuario usuario, String newPass) {
         SimpleMailMessage sm = new SimpleMailMessage();
-        sm.setTo(usuario.getUsuEmail());
+        sm.setTo(usuario.getEmail());
         sm.setFrom(sender);
         sm.setSubject("Solicitação de nova senha");
         sm.setSentDate(new Date(System.currentTimeMillis()));
